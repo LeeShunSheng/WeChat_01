36e98fda741d6b773ba4e98bc7ce0748ef3a6707
LeeShunSheng <1837417085@qq.com>
2021-03-03 21:55:31 +0800

    完成profile页面

--- WeChat_03/pages/profile/profile.js
+++ WeChat_03/pages/profile/profile.js
@@ -1,23 +1,84 @@
-// pages/profile/profile.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    items:[
+      {
+        title:'观影分析',
+        detail:'标记10部影片，开启观影分析',
+        count:'0',
+        icon:'icon-yingpian'
+      },
+      {
+        title:'听歌分析',
+        detail:'标记10首歌曲，开启听歌分析',
+        count:'0',
+        icon:'icon-yinle'
+      },
+      {
+        title:'读书分析',
+        detail:'标记10本书籍，开启观读书分析',
+        count:'0' ,
+        icon:'icon-shu' 
+      }
+    ]
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    
+  },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
